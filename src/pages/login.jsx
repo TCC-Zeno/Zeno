3c70ee2dd990e355b2a00eadddf5d3794bed1841
Vinicius Porto <viniciusporto010@gmail.com>
2025-03-15 19:49:37 -0300

    Deixando particulas mais otimizadas

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -16,8 +16,18 @@ export default function Login() {
         }`}
       >
         <div className={S.particlesContainer}>
-          {Array.from({ length: 1000 }).map((_, i) => (
-            <div key={i} className={S.particle}></div>
+          {Array.from({ length: 100 }).map((_, i) => (
+            <div
+              key={i}
+              className={S.particle}
+              style={{
+                left: `${Math.random() * 100}%`,
+                top: `${Math.random() * 100}%`,
+                "--tx": `${(Math.random() - 0.5) * 200}px`,
+                "--ty": `${(Math.random() - 0.5) * 200}px`,
+                animationDuration: `${Math.random() * 10 + 10}s`,
+              }}
+            ></div>
           ))}
         </div>
 
