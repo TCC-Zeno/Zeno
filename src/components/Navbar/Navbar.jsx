ec9d0233de5fe5263142b3d78cae1d9fa9756125
NicollasMSR <santosreisn997@gmail.com>
2025-03-18 08:48:56 -0300

    só falta a logo no centro

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,80 +1,65 @@
 import React from "react";
 import styles from "./navbar.module.css";
+import { Link } from "react-router-dom";
 import { BsFillHouseFill } from "react-icons/bs";
 import { GiCardboardBoxClosed } from "react-icons/gi";
-import { BsCoin } from "react-icons/bs";
+import { TbCoinFilled } from "react-icons/tb";
 import { FaCalendarAlt } from "react-icons/fa";
-import { BsListCheck } from "react-icons/bs";
-import { TbReportAnalytics } from "react-icons/tb";
-import { MdOutlineHomeRepairService } from "react-icons/md";
-import { GoGear } from "react-icons/go";
+import { PiListChecksFill } from "react-icons/pi";
+import { HiDocumentReport } from "react-icons/hi";
+import { MdHomeRepairService } from "react-icons/md";
+import { FaGear } from "react-icons/fa6";
 import { BiSupport } from "react-icons/bi";
-// import { Logo } from "../../assets/logo/LogoZeno_LogoPretoSFundo.png";
+import Logo from "../../assets/logo/LogoZeno_LogoPretoSFundo.png";
 
 export function Navbar() {
   return (
-    <header>
-      <nav className={styles.navbar}>
-        <div className={styles.nav_brand}>
-          <img className={styles.logo} src="" alt="" />
-          <ul className={styles.ul}>
-            <li>
-              <a className={styles.icons}>
-                <BsFillHouseFill />
-              </a>
-              <a href="/">Inicio</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <GiCardboardBoxClosed />
-              </a>
-              <a href="/">Estoque</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <BsCoin />
-              </a>
-              <a href="/">Fluxo de caixa</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <FaCalendarAlt />
-              </a>
-              <a href="/">Agenda</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <BsListCheck />
-              </a>
-              <a href="/">Organizador</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <TbReportAnalytics />
-              </a>
-              <a href="/">Relatório</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <MdOutlineHomeRepairService />
-              </a>
-              <a href="/">Serviços</a>
-            </li>
-            <li>
-              <a className={styles.icons}>
-                <GoGear />
-              </a>
-              <a href="/">Personalizar</a>
-            </li>
-            <li className={styles.help}>
-              <a className={styles.icons}>
-                <BiSupport />
-              </a>
-              <a href="/">Ajuda</a>
-            </li>
-          </ul>
+    <nav className={styles.navbar}>
+      <div>
+        <img className={styles.logo} src={Logo} alt="" />
         </div>
-      </nav>
-    </header>
+        <div>
+        <ul>
+          <Link className={styles.nav_link} to="/dashboard">
+            <BsFillHouseFill className={styles.icons} />
+            <span>Inicio</span>
+          </Link>
+          <Link className={styles.nav_link} to="/stock">
+            <GiCardboardBoxClosed className={styles.icons} />
+            <span>Estoque</span>
+          </Link>
+          <Link className={styles.nav_link} to="/finance">
+            <TbCoinFilled className={styles.icons} />
+            <span>Fluxo de caixa</span>
+          </Link>
+          <Link className={styles.nav_link} to="/calendar">
+            <FaCalendarAlt className={styles.icons} />
+            <span>Agenda</span>
+          </Link>
+          <Link className={styles.nav_link} to="/list">
+            < PiListChecksFill className={styles.icons} />
+            <span>Organizador</span>
+          </Link>
+          <Link className={styles.nav_link} to="/report">
+            <HiDocumentReport className={styles.icons} />
+            <span>Relatório</span>
+          </Link>
+          <Link className={styles.nav_link} to="/service">
+            <MdHomeRepairService className={styles.icons} />
+            <span>Serviços</span>
+          </Link>
+          <Link className={styles.nav_link} to="/settings">
+            <FaGear className={styles.icons} />
+            <span>Personalizar</span>
+          </Link>
+        </ul>
+      </div>
+      <div className={styles.help}>
+          <Link className={styles.nav_link} to="/support">
+            <BiSupport className={styles.icons} />
+            <span>Ajuda</span>
+          </Link>
+      </div>
+    </nav>
   );
 }
