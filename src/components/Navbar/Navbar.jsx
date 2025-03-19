503108959e6fa7fbabf9f79a8e4e5d814189e6a0
Nicollas Miguel dos Santos Reis <santosreisn997@gmail.com>
2025-03-19 20:49:09 -0300

    testes que não deram certo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,60 +11,117 @@ import { MdHomeRepairService } from "react-icons/md";
 import { FaGear } from "react-icons/fa6";
 import { BiSupport } from "react-icons/bi";
 import { TfiMenu } from "react-icons/tfi";
+import { AiOutlineDoubleLeft } from "react-icons/ai";
 import Logo from "../../assets/logo/LogoZeno_LogoPretoSFundo.png";
 import { useSelector } from "react-redux";
+// import { useRef } from "react";
 
 export function Navbar() {
-
   const rotaStatus = useSelector((state) => state.rotaReducer.rota);
+  // const navRef = useRef();
+  // const showNavbar = () => {
+  //   navRef.current.classList.toggle("reponsive_nav");
+  // };
   return (
-    <nav className={styles.navbar}>
-      <TfiMenu className={styles.menu}/>
-      <div className={styles.containerLogo}>
-        <img className={styles.logo} src={Logo} alt="" />
+    <>
+      <nav className={styles.navbar}>
+        <div className={styles.containerLogo}>
+          <img className={styles.logo} src={Logo} alt="" />
         </div>
         <div>
-        <ul>
-          <Link className={`${styles.nav_link} ${rotaStatus == "dashboard" ? styles.active : null}`} to="/dashboard" >
-            <BsFillHouseFill className={styles.icons} />
-            <span>Inicio</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "stock" ? styles.active : null}`} to="/stock">
-            <GiCardboardBoxClosed className={styles.icons}/>
-            <span>Estoque</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "finance" ? styles.active : null}`} to="/finance">
-            <TbCoinFilled className={styles.icons} />
-            <span>Fluxo de caixa</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "calendar" ? styles.active : null}`} to="/calendar">
-            <FaCalendarAlt className={styles.icons} />
-            <span>Agenda</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "list" ? styles.active : null}`} to="/list">
-            < PiListChecksFill className={styles.icons} />
-            <span>Organizador</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "report" ? styles.active : null}`} to="/report">
-            <HiDocumentReport className={styles.icons} />
-            <span>Relatório</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "service" ? styles.active : null}`} to="/service">
-            <MdHomeRepairService className={styles.icons} />
-            <span>Serviços</span>
-          </Link>
-          <Link className={`${styles.nav_link} ${rotaStatus == "settings" ? styles.active : null}`} to="/settings">
-            <FaGear className={styles.icons} />
-            <span>Personalizar</span>
-          </Link>
-        </ul>
-      </div>
-      <div className={styles.help}>
-          <Link className={`${styles.nav_link} ${rotaStatus == "support" ? styles.active : null}`} to="/support">
+          <ul>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "dashboard" ? styles.active : null
+              }`}
+              to="/dashboard"
+            >
+              <BsFillHouseFill className={styles.icons} />
+              <span>Inicio</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "stock" ? styles.active : null
+              }`}
+              to="/stock"
+            >
+              <GiCardboardBoxClosed className={styles.icons} />
+              <span>Estoque</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "finance" ? styles.active : null
+              }`}
+              to="/finance"
+            >
+              <TbCoinFilled className={styles.icons} />
+              <span>Fluxo de caixa</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "calendar" ? styles.active : null
+              }`}
+              to="/calendar"
+            >
+              <FaCalendarAlt className={styles.icons} />
+              <span>Agenda</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "list" ? styles.active : null
+              }`}
+              to="/list"
+            >
+              <PiListChecksFill className={styles.icons} />
+              <span>Organizador</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "report" ? styles.active : null
+              }`}
+              to="/report"
+            >
+              <HiDocumentReport className={styles.icons} />
+              <span>Relatório</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "service" ? styles.active : null
+              }`}
+              to="/service"
+            >
+              <MdHomeRepairService className={styles.icons} />
+              <span>Serviços</span>
+            </Link>
+            <Link
+              className={`${styles.nav_link} ${
+                rotaStatus == "settings" ? styles.active : null
+              }`}
+              to="/settings"
+            >
+              <FaGear className={styles.icons} />
+              <span>Personalizar</span>
+            </Link>
+          </ul>
+        </div>
+        <div className={styles.help}>
+          <Link
+            className={`${styles.nav_link} ${
+              rotaStatus == "support" ? styles.active : null
+            }`}
+            to="/support"
+          >
             <BiSupport className={styles.icons} />
             <span>Ajuda</span>
           </Link>
-      </div>
-    </nav>
+        </div>
+        {/* <button onClick={showNavbar}>
+          <TfiMenu className={styles.menuClose} />
+        </button>
+        <button onClick={showNavbar}>
+          <AiOutlineDoubleLeft className={styles.menu} />
+        </button> */}
+      </nav>
+    </>
   );
 }
