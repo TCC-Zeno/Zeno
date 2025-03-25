68cf9f2a63b8be9c22db10041094a06e12924fe9
Vinicius Porto <viniciusporto010@gmail.com>
2025-03-25 08:49:36 -0300

    adicionando começo de settings e desenvolvimento de footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,31 @@
 import React from "react";
 import style from "./footer.module.css";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
+import { VscGithubAlt } from "react-icons/vsc";
+import Logo from "../../assets/logo/LogoZeno_LogoPretoSFundo.png"
 
 export function Footer() {
   return (
     <>
       <div className={style.container}>
-        <h4>footer em desenvolvimento</h4>
-        <h4>testando as coisas para o footer</h4>
-        <h4>footer </h4>
-        <h4>footer </h4>
+        <div className={style.footer}>
+        <div className={style.containerLogo}>
+          <img src={Logo} alt="Zeno logo" />
+        </div>
+        <div className={style.conteudo}>
+          <a className={style.links} href="terms">Termos de Serviço</a>
+          <a className={style.links} href="about">Sobre Nós</a>
+          <a className={style.links} href="support">Fale Conosco</a>
+        </div>
+        <div className={style.icons}>
+          <a href="https://facebook.com"><FaFacebookF/></a>
+          <a href="https://instagram.com"><FaInstagram/></a>
+          <a href="https://youtube.com"><FaYoutube/></a>
+          <a href="https://github.com"><VscGithubAlt/></a>
+        </div>
+        </div>
       </div>
     </>
   );
--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -2,17 +2,53 @@ import { useDispatch } from "react-redux";
 import DefaultLayout from "../Layout/DefaultLayout/DefaultLayout";
 import { useEffect } from "react";
 import { settings } from "../redux/Route/slice";
+import S from "./../styles/settings.module.css";
+import { useForm } from "react-hook-form";
 
 export default function Settings() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-      dispatch(settings());
-    }, [dispatch]);
-    return (
-        <DefaultLayout>
-            <div className="container">
-                aaaaaaaaaa
-            </div>
-        </DefaultLayout>
-    )
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(settings());
+  }, [dispatch]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <DefaultLayout>
+      <section className={S.sectionSettings}>
+        <h1>Personalize seu sistema</h1>
+        <div className={S.containerForm}>
+          <form className={S.formSettings} onSubmit={handleSubmit(onSubmit)}>
+            <input
+              className={S.inputCompany}
+              type="text"
+              placeholder="Nome da empresa"
+              {...register}
+            />
+            <input
+              className={S.inputName}
+              type="text"
+              placeholder="Nome do dono da empresa"
+              {...register}
+            />
+            <select className={S.selectColorBlindness}{...register("Select daltonismo")}>
+              <option value="Padrão">Padrão</option>
+              <option value="Protanopia">Protanopia</option>
+              <option value="Deuteranopia">Deuteranopia</option>
+              <option value="Tritanopia">Tritanopia</option>
+              <option value="Achromatopsia">Achromatopsia</option>
+            </select>
+
+            <input type="submit" />
+          </form>
+        </div>
+      </section>
+    </DefaultLayout>
+  );
 }
