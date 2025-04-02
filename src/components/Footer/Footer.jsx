045e6016ea6e77829f7c25700d74e80ad342548f
Nicollas Miguel dos Santos Reis <santosreisn997@gmail.com>
2025-04-02 19:03:35 -0300

    alguns ajustes do teste

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,27 +4,38 @@ import { FaFacebookF } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa6";
 import { VscGithubAlt } from "react-icons/vsc";
-import Logo from "../../assets/logo/LogoZeno_LogoPretoSFundo.png"
+import Logo from "../../assets/logo/LogoZeno_LogoPretoSFundo.png";
 
 export function Footer() {
   return (
     <>
       <div className={style.container}>
         <div className={style.footer}>
-        <div className={style.containerLogo}>
-          <img className={style.img} src={Logo} alt="Zeno logo" />
-        </div>
-        <div className={style.conteudo}>
-          <a className={style.links} href="terms">Termos de Serviço</a>
-          <a className={style.links} href="about">Sobre Nós</a>
-          <a className={style.links} href="support">Fale Conosco</a>
-        </div>
-        <div className={style.icons}>
-          <a  href="https://facebook.com"><FaFacebookF className={style.a}/></a>
-          <a  href="https://instagram.com"><FaInstagram className={style.a}/></a>
-          <a  href="https://youtube.com"><FaYoutube className={style.a}/></a>
-          <a  href="https://github.com"><VscGithubAlt className={style.a}/></a>
-        </div>
+          <div className={style.containerLogo}>
+            <img className={style.img} src={Logo} alt="Zeno logo" />
+          </div>
+          <div className={style.conteudo}>
+            <a className={style.links} href="terms">
+              Termos de Serviço
+            </a>
+            <a className={style.links} href="about">
+              Sobre Nós
+            </a>
+          </div>
+          <div className={style.icons}>
+            <a href="https://facebook.com">
+              <FaFacebookF className={style.a} />
+            </a>
+            <a href="https://instagram.com">
+              <FaInstagram className={style.a} />
+            </a>
+            <a href="https://youtube.com">
+              <FaYoutube className={style.a} />
+            </a>
+            <a href="https://github.com">
+              <VscGithubAlt className={style.a} />
+            </a>
+          </div>
         </div>
       </div>
     </>
