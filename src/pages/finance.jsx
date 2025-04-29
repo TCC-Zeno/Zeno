59220ba5ccac7369ec6e9c453560410a50be8fef
Vinicius Porto <viniciusporto010@gmail.com>
2025-04-29 08:37:54 -0300

    começo do header e arrumando o footer by Nicollas

--- src/pages/finance.jsx
+++ src/pages/finance.jsx
@@ -57,16 +57,53 @@ export default function Finance() {
           </div>
         </div>
         <div className={style.line}></div>
+        <div className={style.row02}>
+          <select
+            className={style.financeSelect1}
+            {...register("Payment method", { required: true })}
+          >
+            <option value="Método de pagamento" disabled selected>
+              Método de pagamento
+            </option>
+            <option value="Cartão de crédito">Cartão de crédito</option>
+            <option value="Cartão de débito">Cartão de débito</option>
+            <option value="Dinheiro">Dinheiro</option>
+            <option value="Pix">Pix</option>
+            <option value="Outros">Outros</option>
+          </select>
+          <select
+            className={style.financeSelect1}
+            {...register("category", { required: true })}
+          >
+            <option value="Categorias" disabled selected>
+              Categorias
+            </option>
+            <option value="Compras">Compras</option>
+            <option value="Contas">Contas</option>
+            <option value="Manutenção">Manutenção</option>
+            <option value="Outros">Outros</option>
+          </select>
+          <select
+            className={style.financeSelect1}
+            {...register("flow", { required: true })}
+          >
+            <option value="Tipo de fluxo" disabled selected>
+              Tipo de fluxo
+            </option>
+            <option value="Entrada">Entrada</option>
+            <option value="Saída">Saída</option>
+          </select>
+        </div>
         <div className={style.containerTable}>
           <table className={style.table}>
             <thead className={style.thead}>
               <tr className={style.tr}>
-                <th>Nome</th>
-                <th>Valor(R$)</th>
-                <th>Metodo de Pagamento</th>
-                <th>Categoria</th>
-                <th>Tipo de Fluxo</th>
-                <th>Ações</th>
+                <th className={style.th}>Nome</th>
+                <th className={style.th}>Valor(R$)</th>
+                <th className={style.th}>Metodo de Pagamento</th>
+                <th className={style.th}>Categoria</th>
+                <th className={style.th}>Tipo de Fluxo</th>
+                <th className={style.th}>Ações</th>
               </tr>
             </thead>
             <tbody>
@@ -185,7 +222,7 @@ export default function Finance() {
                   placeholder="Nome da categoria"
                   {...register("Full name", { required: true })}
                 />
-                <input className={style.button} type="submit" />
+                <input className={style.button2} type="submit" />
               </div>
             </form>
           </div>
