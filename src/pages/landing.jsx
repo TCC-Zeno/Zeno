635d53ed30f9ef13d5764783b6ac722dd61c4a49
Vinicius Porto <viniciusporto010@gmail.com>
2025-05-13 17:18:29 -0300

    Adicionando mais conteudo na landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,16 +1,50 @@
 import { Link } from "react-router-dom";
 import Logo from "./../assets/logo/LogoZeno_LogoBrancoSFundo.png";
 import img001 from "./../assets/tempLanding.png";
-import video001 from "./../assets/3099938-hd_1920_1080_30fps.mp4";
-import { FaBoxes } from "react-icons/fa";
+import video001 from "./../assets/videoDasCores.mp4";
+import video002 from "./../assets/videoDasTelas.mp4";
 import { HiTrendingUp } from "react-icons/hi";
 import { FaCalendarAlt } from "react-icons/fa";
 import { FaClipboardList } from "react-icons/fa";
-import { GiTakeMyMoney } from "react-icons/gi";
 
 import S from "./../styles/landing.module.css";
+import Exemplo from "./../assets/temp.jpg";
+import BlurText from "../components/BlurText/BlurText";
+import { Footer } from "../components/Footer/Footer";
+import { BsFillBoxSeamFill, BsSuitcaseLgFill } from "react-icons/bs";
+import { useEffect } from "react";
 
 export default function LandingPage() {
+  useEffect(() => {
+    const handleClick = (e) => {
+      e.preventDefault();
+      const targetId = e.currentTarget.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        const targetRect = targetElement.getBoundingClientRect();
+        const targetPosition = targetRect.top + window.scrollY;
+        const offsetPosition =
+          targetPosition -
+          window.innerHeight / 2 +
+          targetElement.clientHeight / 2;
+
+        window.scrollTo({
+          top: offsetPosition - 100,
+          behavior: "smooth",
+        });
+      }
+    };
+
+    const anchors = document.querySelectorAll('nav a[href^="#"]');
+    anchors.forEach((anchor) => anchor.addEventListener("click", handleClick));
+
+    return () => {
+      anchors.forEach((anchor) =>
+        anchor.removeEventListener("click", handleClick)
+      );
+    };
+  }, []);
   return (
     <>
       <nav className={S.navbar}>
@@ -23,13 +57,13 @@ export default function LandingPage() {
               <a href="#hero">Inicio</a>
             </li>
             <li>
-              <a href="">Sobre</a>
+              <a href="#function">Sobre</a>
             </li>
             <li>
-              <a href="">Funções</a>
+              <a href="#guide">Funções</a>
             </li>
             <li>
-              <a href="">Personalização</a>
+              <a href="#settings">Personalização</a>
             </li>
             <li>
               <a href="">Sobre nós</a>
@@ -46,12 +80,13 @@ export default function LandingPage() {
         </div>
       </nav>
       <main className={S.page}>
-        <section className={S.hero}>
+        <section id="hero" className={S.hero}>
           <div className={S.titleContainer}>
-            <h1>
-              O sistema que você precisa para <span>consolidar</span> a sua
-              microempresa
-            </h1>
+            <BlurText delay={150} direction="top" className={S.title}>
+              {"O sistema que você precisa para "}
+              <span className={S.spanTitle}>consolidar</span>
+              {"a sua microempresa"}
+            </BlurText>
             <p>
               Comece a sua empresa com melhor que tem para um microempreendedor
             </p>
@@ -63,9 +98,9 @@ export default function LandingPage() {
             <img src={img001} alt="Mockup do sistema" />
           </div>
         </section>
-        <section className={S.function}>
+        <section id="function" className={S.function}>
           <div className={S.functionCard}>
-            <FaBoxes className={S.icon} />
+            <BsFillBoxSeamFill className={S.icon} />
             <div className={S.text}>
               <h2>Controle de estoque</h2>
               <p>Gerencie seu estoque da forma mais adequada.</p>
@@ -102,7 +137,8 @@ export default function LandingPage() {
             </div>
           </div>
           <div className={S.functionCard}>
-            <GiTakeMyMoney className={S.icon} />
+            <BsSuitcaseLgFill className={S.icon} />
+
             <div className={S.text}>
               <h2>Serviços</h2>
               <p>
@@ -113,11 +149,13 @@ export default function LandingPage() {
             </div>
           </div>
         </section>
-        <section className={S.guide}>
+        <section id="guide" className={S.guide}>
           <div className={S.content}>
-            <h1>
-              O Sistema Simples e <span>Poderoso</span> para Seu Negócio
-            </h1>
+            <BlurText delay={150} direction="top" className={S.title}>
+              {"O Sistema Simples e "}
+              <span className={S.spanTitle}>Poderoso</span>
+              {"para Seu Negócio"}
+            </BlurText>
             <p>
               O Zeno foi criado para ajudar microempreendedores a cuidarem de
               tudo com facilidade. Com telas intuitivas e ferramentas
@@ -153,23 +191,77 @@ export default function LandingPage() {
             </div>
           </div>
         </section>
-        <section className={S.settings}>
+        <section id="settings" className={S.settings}>
           <div className={S.videoContainer}>
             <video controls autoPlay loop muted>
-              <source src={video001} type="video/mp4" />
+              <source src={video002} type="video/mp4" />
             </video>
           </div>
           <div className={S.textContainer}>
-            <h1>Seu sistema, do seu  jeito!</h1>
+            <BlurText delay={150} direction="top" className={S.title}>
+              Seu sistema, do seu jeito!
+            </BlurText>
             <p>
               Personalize cores, logo e detalhes para que o Zeno tenha a cara do
               seu negócio.
             </p>
           </div>
         </section>
+        <section id="callAction" className={S.callAction}>
+          <div className={S.separator}></div>
+          <BlurText delay={150} direction="top" className={S.title}>
+            {"Se você quer dar um"}
+            <span className={S.spanTitle}>UP</span>
+            {"no seu pequeno negócio, crie sua conta agora."}
+          </BlurText>
+        </section>
+        <section className={S.aboutUs}>
+          <h1>Sobre nós</h1>
+          <p>
+            Somos um grupo de estudantes da ETEC Martinho Di Ciero e criamos o
+            Zeno, um sistema para MEIs que simplifica a administração de
+            pequenos negócios. Como parte do nosso TCC, desenvolvemos uma
+            plataforma intuitiva com controle de caixa, gestão de estoque,
+            agenda e muito mais. Nosso objetivo? Tornar a tecnologia acessível e
+            eficiente, ajudando empreendedores a focarem no crescimento sem
+            complicação. Transformamos desafios em soluções para fazer seu
+            negócio prosperar!
+          </p>
+          <div className={S.wrapperDevs}>
+            <div className={S.containerDevs}>
+              <img src={Exemplo} alt="Zeno logo" />
+              <h6>Jose Marguarido</h6>
+              <p>Uma pessoa muito boa</p>
+            </div>
+            <div className={S.containerDevs}>
+              <img src={Exemplo} alt="Zeno logo" />
+              <h6>Jose Marguarido</h6>
+              <p>Uma pessoa muito boa</p>
+            </div>
+            <div className={S.containerDevs}>
+              <img src={Exemplo} alt="Zeno logo" />
+              <h6>Jose Marguarido</h6>
+              <p>Uma pessoa muito boa</p>
+            </div>
+            <div className={S.containerDevs}>
+              <img src={Exemplo} alt="Zeno logo" />
+              <h6>Jose Marguarido</h6>
+              <p>Uma pessoa muito boa</p>
+            </div>
+            <div className={S.containerDevs}>
+              <img src={Exemplo} alt="Zeno logo" />
+              <h6>Jose Marguarido</h6>
+              <p>Uma pessoa muito boa</p>
+            </div>
+            <div className={S.containerDevs}>
+              <img src={Exemplo} alt="Zeno logo" />
+              <h6>Jose Marguarido</h6>
+              <p>Uma pessoa muito boa</p>
+            </div>
+          </div>
+        </section>
+        <Footer />
       </main>
-      <h1>Nosso site de apresentação</h1>
-      <Link to="/login">Login</Link>
     </>
   );
 }
