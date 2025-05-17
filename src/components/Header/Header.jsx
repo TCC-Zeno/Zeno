b7c365ea18ed7405d8bc0f2223d65faca893e120
Vinicius Porto <viniciusporto010@gmail.com>
2025-05-17 17:57:37 -0300

    Coririgindo header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,12 +8,37 @@ import { NotificationContent, ProfileContent } from "./dropdownContents";
 
 export default function Header() {
   const rotaStatus = useSelector((state) => state.rotaReducer.rota);
+
+  function getTitulo(rotaStatus) {
+    switch (rotaStatus) {
+      case "finance":
+        return "Fluxo de Caixa";
+      case "stock":
+        return "Estoque";
+      case "organizer":
+        return "Organizador";
+      case "calendar":
+        return "Agenda";
+      default:
+        return "";
+    }
+  }
+
   return (
-    <header className={`${S.header} ${
-      ["dashboard", "finance", "stock", "agenda", "organizer", "calendar"].includes(rotaStatus) 
-        ? null 
-        : S.none
-    }`}>
+    <header
+      className={`${S.header} ${
+        [
+          "dashboard",
+          "finance",
+          "stock",
+          "agenda",
+          "organizer",
+          "calendar",
+        ].includes(rotaStatus)
+          ? null
+          : S.none
+      }`}
+    >
       <div
         className={`${S.containerLogo} ${
           rotaStatus === "dashboard" ? null : S.none
@@ -21,17 +46,20 @@ export default function Header() {
       >
         <img src={Logo} alt="Logo Zeno" />
       </div>
-      <div className={S.title}>
-        <h1>Fluxo de caixa</h1>
+
+      <div
+        className={`${S.title} ${rotaStatus === "dashboard" ? S.none : null}`}
+      >
+        <h1>{getTitulo(rotaStatus)}</h1>
       </div>
       <div className={S.containerIcons}>
         <DropdownHeader
-          icon={<IoMdNotifications className={S.icon}  />}
+          icon={<IoMdNotifications className={S.icon} />}
           modalContent={<NotificationContent />}
         />
 
         <DropdownHeader
-          icon={<CgProfile className={S.icon}  />}
+          icon={<CgProfile className={S.icon} />}
           modalContent={<ProfileContent />}
         />
       </div>
